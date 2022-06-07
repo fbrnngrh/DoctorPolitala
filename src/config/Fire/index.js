e0554b2856d90d8ab8d76d765d11099141dbcc6a
fbrnngrh <fbrnngrh@gmail.com>
2022-06-07 18:13:59 +0800

    first commit

---
+++ src/config/Fire/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCnmeA7dYXsOxkA-TyM33Reo40PFtcPTfc',
+  authDomain: 'klinik-politala.firebaseapp.com',
+  projectId: 'klinik-politala',
+  storageBucket: 'klinik-politala.appspot.com',
+  messagingSenderId: '431382136368',
+  appId: '1:431382136368:web:1a7fcd7007881a58284ff5',
+  measurementId: 'G-3CD9LLZ29S',
+});
+
+const Fire = firebase;
+
+export default Fire;
---
+++ src/redux/store.js
@@ -0,0 +1,27 @@
+import {legacy_createStore as createStore} from 'redux';
+
+const initialState = {
+  loading: false,
+  name: 'Febrian',
+  address: 'Pelaihari',
+};
+
+const reducer = (state = initialState, action) => {
+  if (action.type === 'SET_LOADING') {
+    return {
+      ...state,
+      loading: action.value,
+    };
+  }
+  if (action.type === 'SET_NAME') {
+    return {
+      ...state,
+      name: 'Azzamy',
+    };
+  }
+  return state;
+};
+
+const store = createStore(reducer);
+
+export default store;
